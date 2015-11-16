8a7b152ccdd0352e4021608b15b8667f48babc1c
Ryohei Sonoda <ryoheisonoda@outlook.com>
2015-11-16 22:24:26 +0900

    `%w` is no effect, change to `system` method. Refactoring of such methods split, variable name change.

--- app/helpers/plantuml_helper.rb
+++ app/helpers/plantuml_helper.rb
@@ -1,6 +1,9 @@
 require 'digest/sha2'
 
 module PlantumlHelper
+
+  SOURCE_EXT = '.pu'.freeze
+
   ALLOWED_FORMATS = {
     'png' => { type: 'png', ext: '.png', content_type: 'image/png', inline: true },
     'svg' => { type: 'svg', ext: '.svg', content_type: 'image/svg+xml', inline: true }
@@ -14,27 +17,46 @@ module PlantumlHelper
     ALLOWED_FORMATS.fetch(frmt, ALLOWED_FORMATS['png'])
   end
 
-  def self.plantuml_file(name, extension)
-    File.join(Rails.root, 'files', "#{name}#{extension}")
+  def self.plantuml_file(basename, extension = SOURCE_EXT)
+    File.join(Rails.root, 'files', "#{basename}#{extension}")
+  end
+
+  def self.output_source_file(file, text)
+    File.open(file, 'w') { |w| w.write "@startuml\n#{text}\n@enduml\n" }
+  end
+
+  def self.exec_plantuml(frmt, source_file)
+    output_type = frmt[:type]
+    settings_binary = Setting.plugin_plantuml['plantuml_binary_default']
+    cmd = "#{settings_binary} -charset UTF-8 -t'#{output_type}' #{source_file}"
+    system(cmd)
   end
 
   def self.plantuml(text, args)
+
+    # Generate basename
+    basename = construct_cache_key(text)
+
     frmt = check_format(args)
-    name = construct_cache_key(text)
-    settings_binary = Setting.plugin_plantuml['plantuml_binary_default']
-    if File.file?(plantuml_file(name, '.pu'))
-      unless File.file?(plantuml_file(name, frmt[:ext]))
-        %x("#{settings_binary}" -charset UTF-8 -t"#{frmt[:type]}" "#{plantuml_file(name, '.pu')}")
-      end
-    else
-      File.open(plantuml_file(name, '.pu'), 'w') do |file|
-        file.write "@startuml\n"
-        file.write text + "\n"
-        file.write '@enduml'
-      end
-      %x("#{settings_binary}" -charset UTF-8 -t"#{frmt[:type]}" "#{plantuml_file(name, '.pu')}")
+
+    # If exists image file then finish.
+    if File.file?(plantuml_file(basename, frmt[:ext]))
+      return basename
     end
-    name
+
+    source_file = plantuml_file(basename)
+    
+    # If not exists #{basename}.pu file then create #{basename}.pu file.
+    unless File.file?(source_file)
+      output_source_file(source_file, text)
+    end
+
+    unless exec_plantuml(frmt, source_file)
+      raise "PlantUML execution failed."
+    end
+
+    basename
+
   end
 
 end
